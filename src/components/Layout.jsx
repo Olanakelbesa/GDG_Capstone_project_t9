92ea1b3ebaa29631a837b4a7b21394a4cb460e5b
Olistar <130336147+Olanakelbesa@users.noreply.github.com>
2025-04-14 09:48:42 +0300

    footer update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Footer";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+    
     </>
   );
 };
